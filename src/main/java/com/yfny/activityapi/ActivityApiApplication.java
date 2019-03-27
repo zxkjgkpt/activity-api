16a17bf4d2a3c92f6d2edba08f2553bc08f68cc9
jinboYu <545744706@qq.com>
2019-03-27 09:39:06 +0800

    添加事务处理，添加日志信息存储

--- src/main/java/com/yfny/activityapi/ActivityApiApplication.java
+++ src/main/java/com/yfny/activityapi/ActivityApiApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableEurekaClient
 public class ActivityApiApplication {
--- src/main/java/com/yfny/activityapi/service/CommonService.java
+++ src/main/java/com/yfny/activityapi/service/CommonService.java
@@ -1,108 +1,50 @@
 package com.yfny.activityapi.service;
 
-import com.yfny.activityapi.utils.ActivitiUtils;
-import com.yfny.activityapi.utils.DeleteTaskCmd;
-import com.yfny.activityapi.utils.SetFLowNodeAndGoCmd;
-import org.activiti.bpmn.model.FlowNode;
-import org.activiti.bpmn.model.Process;
-import org.activiti.engine.ManagementService;
-import org.activiti.engine.RepositoryService;
-import org.activiti.engine.TaskService;
-import org.activiti.engine.task.Task;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Map;
 
 /**
  * Activity通用Service
  * <p>
- * Created  by  jinboYu  on  2019/3/11
+ * Created  by  jinboYu  on  2019/3/26
  */
-@Service
-public class CommonService {
-
-    @Autowired
-    private ActivitiUtils activitiUtils;
-
-    @Autowired
-    private TaskService taskService;
-
-    @Autowired
-    private ManagementService managementService;
-
-    @Autowired
-    private RepositoryService repositoryService;
-
+@Transactional(propagation= Propagation.REQUIRED,rollbackFor=Exception.class)
+public interface CommonService {
 
     /**
      * 创建任务
-     * @param userId
-     * @param key
-     * @param variables
-     * @return
+     * @param userId    任务创建人ID
+     * @param key       部署流程ID
+     * @param variables 任务变量
+     * @return          返回任务ID
+     * @throws Exception
      */
-
-    public String createTask(String userId, String key, Map<String,Object> variables){
-        try {
-            //获取当前流程实例ID
-            String processInstanceId =  activitiUtils.getProcessInstance(userId,key).getId();
-            //查询第一个任务
-            Task task = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
-            //设置流程任务变量
-            taskService.setVariables(task.getId(),variables);
-            //完成任务
-            taskService.complete(task.getId());
-            //返回下一个任务的ID
-            String taskId = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult().getId();
-            return "任务ID:"+taskId+",流程实例ID:"+processInstanceId;
-        } catch (Exception e) {
-            return null;
-        }
-    }
+    String createTask(String userId, String key, Map<String, Object> variables) throws Exception;
 
     /**
      * 完成任务
      * @param taskId    任务ID
-     * @param variables 流程变量
-     * @return
+     * @param variables 任务变量
+     * @return          返回任务ID
+     * @throws Exception
      */
-    public String fulfilTask(String taskId, Map<String,Object> variables){
-        try {
-            //根据任务ID获取当前任务实例
-            Task task = this.taskService.createTaskQuery().taskId(taskId).singleResult();
-            //设置流程任务变量
-            taskService.setVariables(taskId,variables);
-            //完成任务
-            taskService.complete(taskId);
-            //返回下一个任务的ID
-            taskId = taskService.createTaskQuery().processInstanceId(task.getProcessInstanceId()).singleResult().getId();
-            return "任务ID:"+taskId+",流程实例ID:"+task.getProcessInstanceId();
-        } catch (Exception e) {
-            return null;
-        }
-    }
+    String fulfilTask(String taskId, Map<String, Object> variables) throws Exception;
 
     /**
      * 取消任务
      * @param taskId    任务ID
-     * @return
+     * @return          1为取消成功，2为取消失败
+     * @throws Exception
      */
-    public int revocationTask(String taskId){
-        try {
-            //获取当前任务
-            Task currentTask = taskService.createTaskQuery().taskId(taskId).singleResult();
-            //获取流程定义
-            Process process = repositoryService.getBpmnModel(currentTask.getProcessDefinitionId()).getMainProcess();
-            //获取目标节点定义
-            FlowNode targetNode = (FlowNode)process.getFlowElement("endevent1");
-            //删除当前运行任务
-            String executionEntityId = managementService.executeCommand(new DeleteTaskCmd(currentTask.getId()));
-            //流程执行到来源节点
-            managementService.executeCommand(new SetFLowNodeAndGoCmd(targetNode, executionEntityId));
-            return 1;
-        } catch (Exception e) {
-            return 0;
-        }
-    }
+    int revocationTask(String taskId) throws Exception;
+
+    /**
+     * 创建用户
+     * @param userId    用户ID
+     * @return          1为创建成功。2为创建失败
+     * @throws Exception
+     */
+    int createUser(String userId) throws Exception;
 }
